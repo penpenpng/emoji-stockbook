b7cb49d66fa44e996ee42e81cbf370cd4d41017e
penpenpng <poman1638m@gmail.com>
2024-03-20 16:54:30 +0900

    create native stockbook dataset

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -1,1 +1,31 @@
-export {};
+import dataByGroup from "unicode-emoji-json/data-by-group.json";
+import keywordsByEmoji from "emojilib";
+import type { EmojiGroup, NativeEmoji } from "@emoji-stockbook/types";
+
+// TODO: Generate at compile-time
+const stockbookDataset = Object.entries(dataByGroup).map(
+  ([groupName, emojis]): EmojiGroup => ({
+    name: groupName,
+    emojis: emojis.map((emoji): NativeEmoji => {
+      const nativeEmoji: NativeEmoji = {
+        shortcode: emoji.slug,
+        char: emoji.emoji,
+      };
+
+      const keywords = (keywordsByEmoji as Record<string, string[]>)[
+        nativeEmoji.char
+      ];
+      if (keywords) {
+        nativeEmoji.keywords = keywords;
+      }
+
+      if (emoji.skin_tone_support) {
+        nativeEmoji.skinToneSupport = true;
+      }
+
+      return nativeEmoji;
+    }),
+  })
+);
+
+export { stockbookDataset };
