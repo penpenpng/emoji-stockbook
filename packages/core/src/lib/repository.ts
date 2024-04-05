4073f5ccc404aff92a73ffed9b2f40b979a434b2
penpenpng <poman1638m@gmail.com>
2024-04-05 22:23:25 +0900

    add EmojiRepository

---
+++ packages/core/src/lib/repository.ts
@@ -0,0 +1,111 @@
+import {
+  Emoji,
+  isEmojiGroups,
+  isNativeEmoji,
+  StockbookData,
+} from "@emoji-stockbook/types";
+
+export class EmojiRepository {
+  #data: StockbookDataModel = { kind: "emojis", emojis: [] };
+  #map: Record<string, EmojiModel> = {};
+
+  constructor(data: StockbookData) {
+    this.setData(data);
+  }
+
+  setData(data: StockbookData) {
+    const map: Record<string, EmojiModel> = {};
+
+    if (isEmojiGroups(data)) {
+      const groups: EmojiGroupModel[] = [];
+
+      for (const { name, emojis } of data) {
+        const emojiModels = EmojiRepository.#modelizeWithMemoizing(emojis, map);
+
+        groups.push({
+          name,
+          emojis: emojiModels,
+        });
+      }
+
+      this.#data = {
+        kind: "groups",
+        groups,
+      };
+    } else {
+      const emojiModels = EmojiRepository.#modelizeWithMemoizing(data, map);
+
+      this.#data = {
+        kind: "emojis",
+        emojis: emojiModels,
+      };
+    }
+
+    this.#map = map;
+  }
+
+  static #modelizeWithMemoizing(
+    emojis: Emoji[],
+    map: Record<string, EmojiModel>
+  ): EmojiModel[] {
+    const emojiModels: EmojiModel[] = [];
+
+    for (const emoji of emojis) {
+      const emojiModel = modelize(emoji);
+      emojiModels.push(emojiModel);
+
+      // memoize
+      if (map[emojiModel.id]) {
+        console.warn(
+          `Emoji IDs are duplicated: ${emojiModel.id}\nThis may lead to unexpected behavior.`
+        );
+      }
+      map[emojiModel.id] = emojiModel;
+    }
+
+    return emojiModels;
+  }
+
+  getAll(): StockbookDataModel {
+    return this.#data;
+  }
+
+  getById(id: string): EmojiModel | undefined {
+    return this.#map[id];
+  }
+
+  search(query: string): EmojiModel[] {
+    const res: EmojiModel[] = [];
+
+    for (const emoji of Object.values(this.#map)) {
+      if (
+        emoji.shortcode.includes(query) ||
+        emoji.keywords?.some((keyword) => keyword.includes(query)) ||
+        (isNativeEmoji(emoji) && emoji.char === query)
+      ) {
+        res.push(emoji);
+      }
+    }
+
+    return res;
+  }
+}
+
+function modelize(emoji: Emoji): EmojiModel {
+  return { ...emoji, id: emoji.id ?? emoji.shortcode };
+}
+
+export type EmojiModel = Readonly<Emoji & { id: string }>;
+export type StockbookDataModel =
+  | {
+      kind: "groups";
+      groups: EmojiGroupModel[];
+    }
+  | {
+      kind: "emojis";
+      emojis: ReadonlyArray<EmojiModel>;
+    };
+export type EmojiGroupModel = Readonly<{
+  name: string;
+  emojis: ReadonlyArray<EmojiModel>;
+}>;
