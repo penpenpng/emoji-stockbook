b4c92c2d7dbcb53b762a82863e76800fc8092acf
penpenpng <poman1638m@gmail.com>
2024-04-05 16:20:52 +0900

    make root element a container

--- packages/core/src/emoji-stockbook.ts
+++ packages/core/src/emoji-stockbook.ts
@@ -1,8 +1,9 @@
 import type { StockbookData } from "@emoji-stockbook/types";
-import { css, html, LitElement } from "lit";
+import { css, html, LitElement, unsafeCSS } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 import { stockbookMain } from "./components/stockbook-main.js";
+import styles from "./emoji-stockbook.css?inline";
 
 /**
  * TODO: https://api-viewer.open-wc.org/docs/guide/writing-jsdoc/
@@ -28,24 +29,39 @@ export class EmojiStockbook extends LitElement {
     const main = stockbookMain({
       data: this.data,
     });
-    const vars = this.#renderDynamicStyles();
+    const styles = this.#renderDynamicStyles();
 
-    return html`${main}${vars}`;
+    return html`${main}${styles}`;
   }
 
+  /*
+    To handle arrow key events, col must be accepted
+    as an attribute, not a CSS variable.
+    Therefore, all style variables that have dependencies
+    with col will be handled by JavaScript.
+  */
   #renderDynamicStyles() {
-    const vars = css`
+    const padding = 20;
+    const defaultWidth =
+      (this.cellSize + this.cellGap) * this.col - this.cellGap + padding * 2;
+
+    // --esb prefix means that the variable is private.
+    const styles = css`
       :host {
-        --col: ${this.col};
-        --cell-size: ${this.cellSize}px;
-        --cell-gap: ${this.cellGap}px;
+        width: var(--emoji-stockbook-width, ${defaultWidth}px);
+
+        --esb-col: ${this.col};
+        --esb-cell-size: ${this.cellSize}px;
+        --esb-cell-gap: ${this.cellGap}px;
       }
     `;
 
     return html`<style>
-      ${vars}
+      ${styles}
     </style>`;
   }
+
+  static styles = unsafeCSS(styles);
 }
 
 declare global {
