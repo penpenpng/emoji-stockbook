3873f4d6d7a47180b2ffde6da2715441af3944e0
penpenpng <poman1638m@gmail.com>
2024-04-05 18:33:07 +0900

    memo

--- packages/core/src/lib/private-component.ts
+++ packages/core/src/lib/private-component.ts
@@ -6,6 +6,10 @@ const PREFIX = "esb-"; // "E"moji "S"tock"B"ook
 export const privateCustomElement = (tagName: string) =>
   customElement(`${PREFIX}${tagName}`);
 
+// TODO:
+// 原則この手のは context で渡すことにすれば、これが必要になるのは emoji-button くらいになるはず
+// render に直接作った DOM を入れるのはパフォーマンス上の問題がありそうなので避けたい
+
 /** Create a short-hand function to render an element using complex property. */
 export const createFactory =
   <E extends LitElement & Props, Props extends object>(Element: new () => E) =>
