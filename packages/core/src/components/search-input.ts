a22432c0cc7b0fefe6c791bd63e67b9e2a2b1310
penpenpng <poman1638m@gmail.com>
2024-04-28 14:09:23 +0900

    implement search feature

--- packages/core/src/components/search-input.ts
+++ packages/core/src/components/search-input.ts
@@ -1,10 +1,21 @@
 import { html, LitElement } from "lit";
 
+import { SearchController } from "../lib/context.js";
 import { privateCustomElement } from "../lib/private-component.js";
 
 @privateCustomElement("search-input")
 export class SearchInput extends LitElement {
+  searchController = new SearchController(this);
+
   render() {
-    return html`<input type="text" />`;
+    return html`<input
+      type="text"
+      .value=${this.searchController.query}
+      @input=${this.onInputQuery}
+    />`;
+  }
+
+  onInputQuery(ev: Event & { target: HTMLInputElement }) {
+    this.searchController.search(ev.target.value);
   }
 }
--- packages/core/src/lib/context.ts
+++ packages/core/src/lib/context.ts
@@ -5,13 +5,15 @@ import { PaletteModel, Stockbook, Unsubscribe } from "./stockbook.js";
 
 export const stockbookContext = createContext<Stockbook>("emoji-stockbook");
 
+export type HostElement = ReactiveControllerHost & HTMLElement;
+
 export class PaletteConsumerController implements ReactiveController {
   #stockbook?: Stockbook;
   #unsubscribe?: Unsubscribe;
   #dispose?: Unsubscribe;
   #value?: PaletteModel;
 
-  constructor(private host: ReactiveControllerHost & HTMLElement) {
+  constructor(private host: HostElement) {
     host.addController(this);
   }
 
@@ -44,5 +46,44 @@ export class PaletteConsumerController implements ReactiveController {
 
   get value() {
     return this.#value;
+  }
+}
+
+export class SearchController implements ReactiveController {
+  #stockbook?: Stockbook;
+  #dispose?: Unsubscribe;
+
+  #query = "";
+
+  constructor(private host: HostElement) {
+    host.addController(this);
+  }
+
+  hostConnected(): void {
+    new ContextConsumer(this.host, {
+      context: stockbookContext,
+      subscribe: true,
+      callback: (stockbook, dispose) => {
+        this.#stockbook = stockbook;
+        this.#dispose = dispose;
+
+        this.search(this.#query);
+      },
+    });
+  }
+
+  hostDisconnected(): void {
+    this.#dispose?.();
+
+    this.#stockbook = undefined;
+  }
+
+  search(query: string) {
+    this.#query = query;
+    this.#stockbook?.search(query);
+  }
+
+  get query() {
+    return this.#query;
   }
 }
